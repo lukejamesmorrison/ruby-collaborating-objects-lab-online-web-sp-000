faf52921a3eedadc0ee6c41216c8407d4922c349
Luke Morrison <lukejamesmorrison@gmail.com>
2020-05-28 20:07:48 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,27 @@
+class Song
+
+  attr_accessor :name, :artist
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    # @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.new_by_filename(filename)
+    file_parts = filename.split(' - '); #0 - artist, 1 - title, 3 - genre.extension
+    song = self.new(file_parts[1])
+    song.artist
+    self.all << song
+  end
+
+  def artist_name
+
+  end
+
+end
